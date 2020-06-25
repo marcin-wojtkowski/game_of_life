49cbbb1e8264a08f18f9c5e9b95571be7ec3129d
Marcin Wojtkowski <marcinwojtkowski94@gmail.com>
2020-06-24 19:09:01 -0700

    Outline

---
+++ stages.py
@@ -0,0 +1,12 @@
+"""
+    Stage 1
+        Learn how to use numpy as a way to create a 2D array
+            -generate a 2D array
+            -change the cells on the 2D array
+
+    Stage 2
+        Populate the board with random alive/dead cells
+            -update a cell without changing the rest of the board
+            -figure out the conditional logic on changing cells 
+
+"""
