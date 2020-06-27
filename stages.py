0f8caa0154b4fe1745cafed0291ae932da196a8e
Marcin Wojtkowski <marcinwojtkowski94@gmail.com>
2020-06-26 20:37:46 -0700

    Revisions to stages

--- stages.py
+++ stages.py
@@ -3,10 +3,22 @@
         Learn how to use numpy as a way to create a 2D array
             -generate a 2D array
             -change the cells on the 2D array
+            #learned how to use numpy
+        ~UP~DATE~
+            Learned how to use numpy and matplotlib. Numpy is used for creating
+            and working with n-dimensional arrays and matplotlib is used for
+            generating scientific/mathematic graphs.
+
+            I will be using pygame because the implementation is simpler to use
+            and the interface is more of what I need; a cartoony graphic
+
+        Learn how to use pygame
+            -create a 2D array using numpy
+            -represent the 2D array graphically using pygame
+            -change the cells on the 2D array, randomly at first
 
     Stage 2
-        Populate the board with random alive/dead cells
-            -update a cell without changing the rest of the board
-            -figure out the conditional logic on changing cells 
+        -implement cell updating between game states 
+        -figure out the conditional logic on changing cells
 
 """
