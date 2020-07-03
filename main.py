09dfa94faff7c56ce4d75964c0c334cd7d4d4097
Marcin Wojtkowski <marcinwojtkowski94@gmail.com>
2020-07-02 21:42:45 -0700

    Drawing 2D array correctly. Randomized grid implemeneted. Main event loop working.

--- main.py
+++ main.py
@@ -5,41 +5,41 @@ import random
 
 size = width, height = 640, 480
 dead = 169, 169, 169
-alive = 55, 255, 0
+alive = 255, 255, 0
 cell_pixel_size = 10
 rows = int(height / cell_pixel_size)
 columns = int(width / cell_pixel_size)
 screen = pygame.display.set_mode(size)
-
-grid = np.zeros((rows, columns))
-
-for r in range(rows):
-    for c in range(columns):
-        grid[r][c] = random.randint(0, 1)
-
-print(grid)
+clock = pygame.time.Clock()
 
 
 pygame.init()
 
-for r in range(rows):
-    for c in range(columns):
-        if grid[r][c] == 0:
-            pygame.draw.rect(screen, dead, (r * 10, c * 10, 10, 10))
-        elif grid[r][c] == 1:
-            pygame.draw.rect(screen, alive, (r * 10, c * 10, 10, 10))
+def create_random_grid():
+    grid = np.zeros((rows, columns))
 
+    for r in range(rows):
+        for c in range(columns):
+            grid[r][c] = random.randint(0, 1)
 
+    return grid
 
+def draw_grid(grid):
+    for r in range(rows):
+        for c in range(columns):
+            if grid[r][c] == 0:
+                pygame.draw.rect(screen, dead, (c * 10, r * 10, 10, 10))
+            elif grid[r][c] == 1:
+                pygame.draw.rect(screen, alive, (c * 10, r * 10, 10, 10))
 
-#pygame.blit()
-
-pygame.display.flip()
-
-
+    pygame.display.flip()
 
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+
+    arg = create_random_grid()
+    draw_grid(arg)
+    clock.tick(2)
